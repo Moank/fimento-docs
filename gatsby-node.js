0bd1e2021e67ebdf8a9da3147ac2272784929c15
Fabrizio Silvestri <fabrizio.silvestri@yahoo.it>
2020-05-25 13:48:26 +0200

    [3397] Add script to fix gadsby/redoc incompatibility

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,18 @@
  */
 
 // You can delete this file if you're not using it
+const path = require('path');
+
+// NOTE: required because redoc is forcing core-js version and
+// gatsby is incompatible with that https://github.com/gatsbyjs/gatsby/issues/17136#issuecomment-568036690
+exports.onCreateWebpackConfig = ({ actions, getConfig, stage }) => {
+  const config = getConfig();
+
+  const coreJs2config = config.resolve.alias['core-js'];
+  delete config.resolve.alias['core-js']
+  config.resolve.alias[`core-js/modules`] = `${coreJs2config}/modules`
+  try {
+    config.resolve.alias[`core-js/es`] = path.dirname(require.resolve('core-js/es'));
+  } catch (err) { }
+  actions.replaceWebpackConfig(config);
+};
