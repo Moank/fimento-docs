43e5e6deb8153e9227f14a6d1221a71097a9e095
Fabrizio Silvestri <fabrizio.silvestri@yahoo.it>
2020-05-25 11:00:45 +0200

    [3397] Update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Fimento | API Docs`,
+    description: `Some fanci description to update`,
+    author: `Fimento`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
