3b79345b7ac6f0ec208f8d608f3019ab2958c1e9
Fabrizio Silvestri <fabrizio.silvestri@yahoo.it>
2020-05-26 10:44:07 +0200

    [3397] Replace gatsby configs back quote

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,35 +1,35 @@
 module.exports = {
   siteMetadata: {
-    title: `Fimento | API Docs`,
-    description: `Some fanci description to update`,
-    author: `Fimento`,
+    title: 'Fimento | API Docs',
+    description: 'Some fanci description to update',
+    author: 'Fimento',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `Fimento`,
-        short_name: `Fimento`,
-        start_url: `/`,
-        background_color: `#f2f0ed`,
-        theme_color: `#DBC9C6`,
-        display: `standalone`,
-        icon: `src/images/logo-filled.svg`,
+        name: 'Fimento',
+        short_name: 'Fimento',
+        start_url: '/',
+        background_color: '#f2f0ed',
+        theme_color: '#DBC9C6',
+        display: 'standalone',
+        icon: 'src/images/logo-filled.svg',
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    // 'gatsby-plugin-offline',
   ],
 }
