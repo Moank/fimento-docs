83c699d7eee5f3ec42911e0485e70286defe10d3
Fabrizio Silvestri <fabrizio.silvestri@yahoo.it>
2020-05-26 18:09:58 +0200

    [3409] Move file in import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,16 @@
 import React from "react"
 import { RedocStandalone } from 'redoc';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+import fimentoDocs from '../docs/fimento-api.yml';
+
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Documentation" />
-    <RedocStandalone specUrl="/docs.yml" />
+    <RedocStandalone specUrl={fimentoDocs} />
   </Layout>
 )
 
